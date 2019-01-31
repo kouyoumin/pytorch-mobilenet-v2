7d8b66a45c3aa7c34a62580d99d0e518878184b8
kouyoumin <kouyoumin@2tvnow.com>
2019-01-31 09:53:27 +0800

    Added CReLU

--- MobileNetV2.py
+++ MobileNetV2.py
@@ -1,13 +1,29 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-def conv_bn(inp, oup, stride):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
+class CReLU6(nn.Module):
+    def __init__(self):
+        super(CReLU6, self).__init__()
+
+    def forward(self, x):
+        return torch.cat((F.relu6(x), F.relu6(-x)), 1)
+
+
+def conv_bn(inp, oup, stride, crelu=False):
+    if crelu:
+        return nn.Sequential(
+            nn.Conv2d(inp, oup//2, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(oup//2),
+            CReLU6()
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(oup),
+            nn.ReLU6(inplace=True)
+        )
 
 
 def conv_1x1_bn(inp, oup):
@@ -19,7 +35,7 @@ def conv_1x1_bn(inp, oup):
 
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio):
+    def __init__(self, inp, oup, stride, expand_ratio, crelu=False):
         super(InvertedResidual, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
@@ -28,29 +44,55 @@ class InvertedResidual(nn.Module):
         self.use_res_connect = self.stride == 1 and inp == oup
 
         if expand_ratio == 1:
-            self.conv = nn.Sequential(
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
+            if crelu:
+                self.conv = nn.Sequential(
+                    # dw
+                    nn.Conv2d(hidden_dim, hidden_dim//2, 3, stride, 1, groups=hidden_dim, bias=False),
+                    nn.BatchNorm2d(hidden_dim//2),
+                    CReLU6(),
+                    # pw-linear
+                    nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(oup),
+                )
+            else:
+                self.conv = nn.Sequential(
+                    # dw
+                    nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                    nn.BatchNorm2d(hidden_dim),
+                    nn.ReLU6(inplace=True),
+                    # pw-linear
+                    nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(oup),
+                )
         else:
-            self.conv = nn.Sequential(
-                # pw
-                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
+            if crelu:
+                self.conv = nn.Sequential(
+                    # pw
+                    nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(hidden_dim),
+                    nn.ReLU6(inplace=True),
+                    # dw
+                    nn.Conv2d(hidden_dim, hidden_dim//2, 3, stride, 1, groups=hidden_dim, bias=False),
+                    nn.BatchNorm2d(hidden_dim//2),
+                    CReLU6(),
+                    # pw-linear
+                    nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(oup),
+                )
+            else:
+                self.conv = nn.Sequential(
+                    # pw
+                    nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(hidden_dim),
+                    nn.ReLU6(inplace=True),
+                    # dw
+                    nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                    nn.BatchNorm2d(hidden_dim),
+                    nn.ReLU6(inplace=True),
+                    # pw-linear
+                    nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                    nn.BatchNorm2d(oup),
+                )
 
     def forward(self, x):
         if self.use_res_connect:
@@ -60,8 +102,9 @@ class InvertedResidual(nn.Module):
 
 
 class MobileNetV2(nn.Module):
-    def __init__(self, n_class=1000, input_size=224, width_mult=1., grayscale=False):
+    def __init__(self, n_class=1000, input_size=224, width_mult=1., dropout_rate=0.2, grayscale=False, crelu=False):
         super(MobileNetV2, self).__init__()
+        self.n_class = n_class
         block = InvertedResidual
         input_channel = 32
         last_channel = 1280
@@ -80,7 +123,7 @@ class MobileNetV2(nn.Module):
         assert input_size % 32 == 0
         input_channel = int(input_channel * width_mult)
         self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
-        self.features = [conv_bn(3 if not grayscale else 1, input_channel, 2)]
+        self.features = [conv_bn(3 if not grayscale else 1, input_channel, 2, crelu=crelu)]
         # building inverted residual blocks
         for t, c, n, s in interverted_residual_setting:
             output_channel = int(c * width_mult)
@@ -97,7 +140,7 @@ class MobileNetV2(nn.Module):
 
         # building classifier
         self.classifier = nn.Sequential(
-            nn.Dropout2d(0.2),
+            nn.Dropout2d(dropout_rate),
             nn.Conv2d(self.last_channel, n_class, 1)
         )
 
@@ -108,9 +151,10 @@ class MobileNetV2(nn.Module):
         if not heatmap:
             x = F.adaptive_avg_pool2d(x, (1, 1))
             x = self.classifier(x).view(x.size(0), -1)
+            return x
         else:
             x = self.classifier(x)
-        return x
+            return F.adaptive_avg_pool2d(x, (1, 1)).view(x.size(0), -1), x
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -126,7 +170,7 @@ class MobileNetV2(nn.Module):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
     
-    def load_state_dict(self, state_dict):
+    def load_state_dict(self, state_dict, strict=True, transfer=False):
         keys = []
         for key in state_dict:
             keys.append(key)
@@ -136,4 +180,13 @@ class MobileNetV2(nn.Module):
                 state_dict[key[7:]] = state_dict[key]
                 del state_dict[key]
         
-        super(MobileNetV2, self).load_state_dict(state_dict)
+        keys = []
+        for key in state_dict:
+            keys.append(key)
+        
+        if transfer:
+            for key in keys:
+                if key.startswith('classifier.'):
+                    del state_dict[key]
+        
+        super(MobileNetV2, self).load_state_dict(state_dict, strict=strict)
